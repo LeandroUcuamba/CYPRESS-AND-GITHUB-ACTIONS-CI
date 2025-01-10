eee46b524347a0a1a7fceaf6fdbda47345532e2d
Leandro Ucuamba <leandrodossantos.lc5@gmail.com>
2025-01-10 21:14:30 +0100

    password fixed

--- cypress/e2e/test.spec.cy.js
+++ cypress/e2e/test.spec.cy.js
@@ -2,7 +2,7 @@ describe('Leandro Ucuamba Test - ', () => {
   it('Automated Test - Github Actions', () => {
     cy.visit("https://practicetestautomation.com/practice-test-login/")
     cy.get("#username").clear().type("student")
-    cy.get("#password").clear().type("Password1234")
+    cy.get("#password").clear().type("Password123")
     cy.get("#submit").click()
     cy.get('#loop-container > div > article > div.post-header > h1').should('be.visible').should('have.text', 'Logged In Successfully');
   })
